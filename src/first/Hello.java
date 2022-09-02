deb6f686c1424fd81b5d21012e8b0a1fb18debcb
lucky <lucky@LAPTOP-H8BM633V>
2022-09-02 15:29:55 +0530

    commite world

--- src/first/Hello.java
+++ src/first/Hello.java
@@ -1,6 +1,40 @@
-package first;
 
-public class Hello {
+
+class integerdatatypes 
+{
+	public static void main(String args) {
+		
+		int a =  10;
+		
+		short s  =  2;
+		
+		byte b  = 6;
+		
+		long  l = 12345678334531;
+		
+		char c = 'l';
+		
+		float f = 1.64;
+		
+		system.out.println("the int variable is" + a + '/n');
+		
+		System.out.println(" the short variable is" + s + '/n');
+		
+		System.out.println(" the byte variable is" + b +'/n');
+		
+		System.out.println("the long variable is" + l + '/n');
+		
+		systme.out.println(" the char value is" + c + '/n');
+		
+		systme.out.println("the float value is + f +" '/n);
+				
+	}
+	
+}
+		
+		
+		
+	}
   public static void main(String[] args)
   {
 	  System.out.println("k.CHINDU BABU");
